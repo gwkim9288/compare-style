1720e10e56c2246a5ae51fa8acce71f06b3e4600
kiseok Nam <62214428+skarltjr@users.noreply.github.com>
2021-04-12 21:01:23 +0900

    feat-first/accountRepo (#10)

--- src/main/java/com/example/comparestyle/accounts/AccountRepository.java
+++ src/main/java/com/example/comparestyle/accounts/AccountRepository.java
@@ -7,4 +7,5 @@ import org.springframework.transaction.annotation.Transactional;
 public interface AccountRepository extends JpaRepository<Account,Long> {
 
     Account findByUsername(String username);
+    Account findByUsername2(String username);
 }
